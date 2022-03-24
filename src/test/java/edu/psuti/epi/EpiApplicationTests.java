611e80b980612d6dbc34caf13086fde9dbd65765
Ilya <180229@edu.psuti.ru>
2022-03-24 11:14:24 +0400

    Initial commit

---
+++ src/test/java/edu/psuti/epi/EpiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.psuti.epi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
